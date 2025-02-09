58af7f77372e18f4eabab4d9c4e3b4087e0c9511
21bcs6094 <21bcs6094@cuchd.in>
2025-02-09 11:53:41 +0530

    docs: code update

--- demo/src/main/java/com/example/demo/exceptions/EmailAlreadyExists.java
+++ demo/src/main/java/com/example/demo/exceptions/EmailAlreadyExists.java
@@ -3,9 +3,9 @@ package com.example.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
+@ResponseStatus(value = HttpStatus.ALREADY_REPORTED)
+public class EmailAlreadyExits extends IllegalArgumentException {
+    public EmailAlreadyExits(String message) {
         super(message);
     }
 }
---
+++ demo/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmailAlreadyExits.class)
+    public ResponseEntity<String> handleEmailAlreadyExists(EmailAlreadyExits ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
