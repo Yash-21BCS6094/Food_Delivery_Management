74127e0e8db7c7e16a58b7639d238afee98e00ad
21bcs6094 <21bcs6094@cuchd.in>
2025-02-09 11:47:43 +0530

    docs: code update

---
+++ demo/src/main/java/com/example/demo/entities/Address.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String city;
+
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
